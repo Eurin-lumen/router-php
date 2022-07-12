693d3c64788cc083d6f25c4b778ce44e3d596719
eurin-lumen <eurindelagoldcoast@gmail.com>
2022-07-12 14:50:43 +0100

    création de router : preg_match()

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-
+//pregmatch
 $url = '';
+if(isset($_GET['url'])){
+    $url = $_GET['url'];
+}
+
+if($url == ''){
+    require 'home.php';
+
+}elseif(preg_match('#article-([0-9]+)#', $url, $params)){
+    //var_dump($params);
+    //require  "home.php";
+    $idArticle =  $params[1];
+    require "article.php";
+}else{
+    require '404.php';
+}
+
+/* $url = '';
 if(isset($_GET['url'])){
     $url = explode('/', $_GET['url']);
 
@@ -15,6 +32,6 @@ if($url == ""){
     require "article.php";
 }else{
     require "404.php";
-
+ 
 }
-
+*/
